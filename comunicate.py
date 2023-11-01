933d12b6cc5a7b52cbfe721aba28c327fd8646ec
Salah eddine berret <146879159+SBerret3301@users.noreply.github.com>
2023-11-01 15:20:57 +0100

    Update comunicate.py

--- comunicate.py
+++ comunicate.py
@@ -9,13 +9,13 @@ def com(m):
         t.append(20)
     a2 = m - 60
     if a2 >= 0 :
-        b2 = 50 + a2 * 1.5
+        b2 = 50 + a2 * 1
         t.append(b2)
     else :
         t.append(50)
     a3 = m - 120
     if a3 >= 0 :
-        b3 = 100 + a3 * 1.5
+        b3 = 100 + a3 * 0.5
         t.append(b3)
     else :
         t.append(100)
